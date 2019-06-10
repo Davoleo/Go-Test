e5f6c9493ddc198eacd86fa3920033027a04050d
Davoleo <davoleo2001@gmail.com>
2019-06-10 17:06:55 +0200

    Redo ex2

--- main/playground-practice/ex2.go
+++ main/playground-practice/ex2.go
@@ -1,7 +1,17 @@
 package playground_practice
 
 func main() {
+	var a, b, c int = RandInt(1, 6), RandInt(1, 6), RandInt(1, 6)
 
-	//I Fucking need to redo it
+	Println("a = ", a)
+	Println("b = ", b)
+	Println("c = ", c)
 
+	if a < b && a < c {
+		Alert("Il numero minore è: ", a)
+	} else if b < c {
+		Alert("Il numero minore è: ", b)
+	} else {
+		Alert("Il numero minore è: ", c)
+	}
 }
