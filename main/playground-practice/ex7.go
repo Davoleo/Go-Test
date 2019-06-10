d11092fcfdc7036bea8cffedd98c8315dd1d185f
Davoleo <davoleo2001@gmail.com>
2019-06-10 18:04:49 +0200

    Ex7 - Calcolo delle resistenze

---
+++ main/playground-practice/ex7.go
@@ -0,0 +1,19 @@
+package playground_practice
+
+func main() {
+
+	resInput := ToFloat(Prompt("Inserisci una resistenza (0 per terminare l'inserimento)"))
+	totalSerie := 0.0
+	totalParallelo := 0.0
+
+	for resInput != 0 {
+		totalSerie += resInput
+		totalParallelo += (1.0 / resInput)
+
+		resInput = ToFloat(Prompt("Inserisci una resistenza (0 per terminare l'inserimento)"))
+	}
+
+	Alert("Resistenza totale in Serie: ", totalSerie)
+	Alert("Resistenza totale in Parallelo: ", totalParallelo)
+
+}
